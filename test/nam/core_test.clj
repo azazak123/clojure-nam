434465fb1b6ac4b582394b5b5953febc0be0b9f4
azazak123 <azazaka2002@gmail.com>
2023-02-07 17:09:43 +0200

    test: add nam function tests

--- test/nam/core_test.clj
+++ test/nam/core_test.clj
@@ -1,7 +1,23 @@
 (ns nam.core-test
   (:require [clojure.test :refer :all]
-            [nam.core :refer :all]))
+            [nam :refer [nam]]
+            [nam.substitution :refer [--> ==>]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest substitute-eq-test
+  (is (let [result (nam "abbbcaa"
+                        "aa" --> "a"
+                        "bb" --> "b"
+                        "cc" --> "c")]
+        (= result "abca"))))
+
+(deftest increment-test
+  (is (let [result (nam "1000"
+                        "*0" --> "0*"
+                        "*1" --> "1*"
+                        "0*" ==> "1"
+                        "1*" --> "/0"
+                        "1/" --> "/0"
+                        "0/" ==> "1"
+                        "/" ==> "1"
+                        "" --> "*")]
+        (= result "1001"))))
